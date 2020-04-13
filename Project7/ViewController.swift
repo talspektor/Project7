a520243091542a609d78213ddc7a2936d6b03d7f
Tal <talspektor.gmail.com>
2020-04-13 07:24:27 +0300

    Initial Commit

---
+++ Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Tal Spektor on 13/04/2020.
+//  Copyright © 2020 Tal Spektor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
